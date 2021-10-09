62347b03bc5188dfa2c6128045e854c2cb96ffbe
Andre Moraes <andrelom@gmail.com>
2021-10-09 00:55:23 -0300

    Update HomeController.cs

--- src/Monolith.Web.Default/Controllers/HomeController.cs
+++ src/Monolith.Web.Default/Controllers/HomeController.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Monolith.Core.Mvc;
+using Monolith.Foundation.Identity;
 
 namespace Monolith.Web.Default.Controllers
 {
     [ApiVersion("1")]
-    [Authorize(Roles = "Administrator")]
+    [Authorize(Roles = Roles.Administrator)]
     public class HomeController : ControllerApi
     {
         [HttpGet]
