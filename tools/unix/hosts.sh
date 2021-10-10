09d586b885c3f32e052bd71c0dc3b231bec6e3bf
Andre Moraes <andrelom@gmail.com>
2021-10-09 22:57:59 -0300

    Create hosts.sh

---
+++ tools/unix/hosts.sh
@@ -0,0 +1,37 @@
+set -o allexport; source .env; set +o allexport
+
+declare -a HOSTS=(
+  "identity.monolith.local"
+  "default.monolith.local"
+)
+
+# Menu
+
+help() {
+  echo ""
+  echo "Usage:"
+  echo "add" "\t" "It add the host entries to /etc/hosts."
+  exit 1
+}
+
+# Tasks
+
+add() {
+  for HOST in "${HOSTS[@]}"
+  do
+    if [ -n "$(grep $HOST /etc/hosts)" ]
+    then
+      echo "The '$HOST' already exists"
+    else
+      echo "Adding $HOST";
+      sudo -- sh -c -e "echo '127.0.0.1\t$HOST' >> /etc/hosts";
+    fi
+  done
+}
+
+# Main
+
+case "${1}" in
+  add ) add  ;;
+  *   ) help ;;
+esac
